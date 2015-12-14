4329af0c9351db81bc4ffe3b0bf089d3f31def7f
Tony Baron <anthony.t.baron@gmail.com>
2015-12-14 15:02:32 -0500

    Updated time filter on sidebar to default endTime as 23:59:59 per feedback as opposed to 00:00:00

--- app/modules/sidebar/temporal/opDateTimeController.js
+++ app/modules/sidebar/temporal/opDateTimeController.js
@@ -37,7 +37,7 @@ angular.module('opApp.sidebar.temporal').controller('opDateTimeController',
         $scope.startDate = moment.utc().startOf('d').format('MM/DD/YYYY');
         $scope.startTime = moment.utc().startOf('d').format('HH:mm:ss');
         $scope.endDate = moment.utc().startOf('d').add('days', 1).format('MM/DD/YYYY');
-        $scope.endTime = moment.utc().startOf('d').add('days', 1).format('HH:mm:ss');
+        $scope.endTime = moment.utc().endOf('d').add('days', 1).format('HH:mm:ss');
 
 
         $scope.rangeTimeout = false;
