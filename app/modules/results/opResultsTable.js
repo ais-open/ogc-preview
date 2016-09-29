93cbdcdf73d0775aac74cd353aa4e3e73b8483cc
root <root@localhost.localdomain>
2016-09-28 19:45:45 -0700

    A whole bunch of new javascript and style files for dataTables update for results table display and interaction.

--- app/modules/results/opResultsTable.js
+++ app/modules/results/opResultsTable.js
@@ -1,30 +1,8 @@
-angular.module('opApp').directive('opResultsTable', ['$timeout',
-    function ($timeout) {
+angular.module('opApp').directive('opResultsTable', ['$timeout', '$window',
+    function ($timeout, $window) {
     'use strict';
 
-    if (typeof window.ColumnsPlugins === 'undefined') {
-        window.ColumnsPlugins = {};
-    }
-
-    window.ColumnsPlugins.resizeable = {
-        init: function () {
-            this.columnData = {
-                data: {},
-                set: function (key, val) {
-                    this.data[key] = val;
-                },
-                get: function (key) {
-                    return this.data[key];
-                }
-            };
-        },
-        create: function () {
-            var $this = this;
-            setTimeout(function () {
-                $($this.$el).find('table').resizableColumns({store: $this.columnData, syncHandlers: false});
-            }, 0);
-        }
-    };
+       
 
     return {
         restrict: 'EA',
@@ -32,37 +10,60 @@ angular.module('opApp').directive('opResultsTable', ['$timeout',
         scope: {
             opModel: '=',
             loading: '=',
-            error: '='
+            error: '=',
+            layer: '='
         },
         link: function (scope, element) {
-            scope.noData = true;
-            var viewTable = null;
-            element.on('click', '.clear', function () {
-                angular.element('.ui-table-search').val('').keyup();
-            });
+            var table = null;
 
             scope.$watch('opModel', function () {
-                if (viewTable) {
-                    viewTable.destroy();
-                }
+                
+                var dataSet = [];
+                
+                
+                if(scope.opModel.length > 0){
+                    var columns = []
+                    var columnKeys = scope.opModel[0];
+                    for(var property in columnKeys){
+                        columns.push({"sTitle":property});
+                    }
 
-                if (scope.opModel.length) {
-                    scope.noData = false;
-                    viewTable = element.find('.data-table').columns({
-                        data: scope.opModel,
-                        paginating: false,
-                        //plugins: ['resizeable'],
-                        template: '{{#search}}<div class="ui-columns-search"> <input class="ui-table-search" placeholder="Filter Results" type="text" name="query" data-columns-search="true" value="{{query}}" required /><div class="clear">&times;</div></div></div>{{/search}} {{#table}}<div class="ui-columns-table" data-columns-table="true"> <table class="display table table-striped table-bordered table-hover"> <thead> {{#headers}}   {{#sortable}} <th class="ui-table-sortable" data-columns-sortby="{{key}}"  data-resizable-column-id="{{header}}">{{header}}</th> {{/sortable}}  {{#notSortable}} <th data-resizable-column-id="{{header}}">{{header}}</th> {{/notSortable}}  {{#sortedUp}} <th class="ui-table-sort-up ui-table-sortable" data-columns-sortby="{{key}}" data-resizable-column-id="{{header}}">{{header}} <span class="ui-arrow">&#x25B2;</span></th> {{/sortedUp}}  {{#sortedDown}} <th class="ui-table-sort-down ui-table-sortable" data-columns-sortby="{{key}}" data-resizable-column-id="{{header}}">{{header}} <span class="ui-arrow">&#x25BC;</span></th> {{/sortedDown}}    {{/headers}} </thead> <tbody> {{#rows}} {{{.}}} {{/rows}} </tbody>  </table> </div>{{/table}}'
+                    if(table != null){
+                        table.fnClearTable();
+                        table.fnDestroy();
+                        $('#table').empty();
+                    }
+                    
+                    table = $('#table').dataTable(
+                    {
+                        "bPaginate": false,
+                        "bLengthChange": false, 
+                        "bDestroy": true,
+                        "bInfo": false,
+                        select: {
+                            style: 'multi+shift'
+                        },
+                        columns: columns
                     });
 
-                    $timeout(function () {
-                        angular.element('.ui-table-search').placeholder();
-                    });
-                } else {
-                    scope.noData = true;
+                    for (var i = 0; i < scope.opModel.length; i++) {
+                        var row = scope.opModel[i];
+                        var item = []
+                    
+                        for(var property in row)
+                            item.push(row[property]);
+                        dataSet.push(item);
+                    }
+                    table.fnAddData(dataSet);   
+
+                    $('#table tbody').on( 'click', 'tr', function () {
+                        var rowData = table.api().rows( {selected:true} ).data();
+                        $window.opener.resultsSelected(rowData);
+                    });       
                 }
             });
-
+            
+            
         }
     };
 }]);
